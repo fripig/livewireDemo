7e7fee64482288a6f2eba935b7568284c3a91811
fripig <fripig@gmail.com>
2023-08-16 12:42:33 +0800

    create Counter inline

---
+++ app/Livewire/CounterInline.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CounterInline extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{-- Nothing in the world is as soft and yielding as water. --}}
+        </div>
+        HTML;
+    }
+}
